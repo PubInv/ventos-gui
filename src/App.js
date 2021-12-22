692717bccea41642615e615cbbc8bec71fff38a4
Erich Schulz <erichbschulz@gmail.com>
2021-12-22 16:29:25 +1000

    merge app into clinical display to flatten tree

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import "./App.css";
-import ClinicalDisplay from "./components/ClinicalDisplay";
-import ServerConfigForm from "./components/ServerConfigForm";
 import { useReducer,
   useEffect,
-  //useState
+  useState
   } from 'react';
 import {server} from './PIRServer'
+import ServerConfigForm from "./components/ServerConfigForm";
+import SettingButton from "./components/settings/SettingButton";
 
 const initialState = {
   display_mode: 'clinical',
   live: true,
   ventilator_session: {}
 };
+
+// dummy data
+const settings = [
+  {name: 'Mode', value: "PCV", units: ""},
+  {name: 'Pmax', value: "20", units: "cmH2O"},
+  {name: 'PEEP', value: "5", units: "cmH2O"},
+  {name: 'RR', value: "11", units: "/min"},
+]
+
+const observations = [
+  {name: 'Ppeak', value: "23", units: "cmH2O"},
+  {name: 'PEEP', value: "5", units: "cmH2O"},
+  {name: 'TV', value: "412", units: "ml"},
+  {name: 'ml', value: "4.8", units: "L"},
+  {name: 'RR', value: "11", units: "/min"},
+]
 
 
 /**
@@ -32,7 +48,14 @@ function reducer(state, action) {
 function App() {
 
   const [state, dispatch] = useReducer(reducer, initialState);
-  console.log('state', JSON.stringify(state, null, 2))
+  // const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [showLeftPanel, setShowLeftPanel] = useState(true);
+
+  function newSetting(field, value) {
+    alert(field, value)
+  }
+
+  console.log('restarting app: state', JSON.stringify(state, null, 2))
 
   useEffect(() => {
     const settings = server.default_ventilator_session
@@ -47,14 +70,52 @@ function App() {
   }
 
   return (
-    <div className="App">
+  <div className="App">
       <h1 className="display-4">VentOS Clinical GUI
-    |{state.ventilator_session.dserverurl} {state.ventilator_session.traceid}|
-    </h1>
-      <ClinicalDisplay />
-      <ServerConfigForm
-         ventilator_session={state.ventilator_session}
-         dispatch={dispatch}/>
+      |{state.ventilator_session.dserverurl} {state.ventilator_session.traceid}|
+      </h1>
+    <div className="flex flex-col w-screen text-green-600 bg-black ">
+      <div className="flex justify-between">
+        {showLeftPanel ? (
+          <div className="mt-4 border-2 border-solid border-cyan-100-accent w-96">
+              <button type="button" className="float-right m-3"
+                onClick={() => setShowLeftPanel(false)} >
+                X
+              </button>
+            <div className="flex justify-between m-2">
+              <ServerConfigForm
+                 ventilator_session={state.ventilator_session}
+                 dispatch={dispatch}/>
+            </div>
+        </div>
+      ) : (
+        <div className="m-4">
+          <button type="button" onClick={() => setShowLeftPanel(true)} >
+            Show
+          </button>
+        </div>
+      )}
+
+      <div id="PFGraph"></div>
+
+      <div className="flex flex-col mt-4 w-48">
+        {observations.map((o) => <div key={o.name}>
+          <div className="flex h-36 w-40rem">
+            <div>
+              <p className="float-left">{o.name}</p>
+              <span className='display-2 text-right'>{o.value}</span>
+              <span className='float-right text-right'>{o.units}</span>
+            </div>
+          </div>
+        </div>)}
+      </div>
+    </div>
+
+    <div className="flex justify-center">
+      {settings.map((s) => <SettingButton key={s.name}
+        setting={s} onChange={newSetting} />)}
+    </div>
+  </div>
     <p className="lead">This is a work in progress.</p>
       <button onClick={() => setServer(!state.live)} >
         {state.live ? 'stop' : 'start'} data fetch
